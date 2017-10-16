8f0afae1371dc5881ee9f2d46290401497c6bfa9
NextZeus <875697126@qq.com>
2017-10-16 22:45:15 +0800

    update

--- Assets/Live/Scripts/ColumnLive.cs
+++ Assets/Live/Scripts/ColumnLive.cs
@@ -5,10 +5,7 @@ using UnityEngine;
 public class ColumnLive : MonoBehaviour {
 	private void OnTriggerEnter2D(Collider2D other)
 	{	
-		Debug.Log("OnTriggerEnter2D");
-		
 		if(other.GetComponent<BirdLive> () != null){
-			Debug.Log("scores update");
 			GameController.instance.BirdScored();
 		}
 	}
